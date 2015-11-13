4905f2e7bdce08f9a55d5b97209c6f35f5cfbf0a
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-11-12 23:10:56 -0500

    Set up sut in root most beforeEach.

--- __tests__/game.spec.js
+++ __tests__/game.spec.js
@@ -1,22 +1,19 @@
 jest.dontMock('./../src/game');
 
 describe("Game", () => {
+	let sut;
+	before(() => {
+		sut = require('./../src/game')();
+	});
 
 	describe("Given a new game", () => {
-		let sut = null;
-		beforeEach(() => {
-			sut = require('./../src/game')();
-		});
-
 		it("Starts with a score of zero", () => {
 			expect(sut.getScore()).toEqual(0);
 		});
 	});
 
 	describe("Given a game with all gutter balls", () => {
-		let sut = null;
 		beforeEach(() => {
-			sut = require('./../src/game')();
 			for(var i = 0; i < 20; i++){
 				sut.roll(0);
 			}
