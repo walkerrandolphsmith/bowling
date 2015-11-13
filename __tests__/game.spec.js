38f20b70c5ff8ec0bf935d1249f7b368ac787589
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-11-12 23:04:52 -0500

    Assert a game with all gutter balls has a score of 0. Nest describe blocks within root most describe block to indicate the Game is the sut.

--- __tests__/game.spec.js
+++ __tests__/game.spec.js
@@ -1,13 +1,29 @@
 jest.dontMock('./../src/game');
 
 describe("Game", () => {
-	let sut = null;
-	beforeEach(() => {
-		sut = require('./../src/game')();
-		console.log("SUT", sut);
+
+	describe("Given a new game", () => {
+		let sut = null;
+		beforeEach(() => {
+			sut = require('./../src/game')();
+		});
+
+		it("Starts with a score of zero", () => {
+			expect(sut.getScore()).toEqual(0);
+		});
 	});
 
-	it("Starts with a score of zero", () => {
-		expect(sut.getScore()).toEqual(0);
+	describe("Given a game with all gutter balls", () => {
+		let sut = null;
+		beforeEach(() => {
+			sut = require('./../src/game')();
+			for(var i = 0; i < 20; i++){
+				sut.roll(0);
+			}
+		});
+
+		it('when calculating the score, then the score should equal 0', () => {
+			expect(sut.getScore()).toEqual(0);
+		});
 	});
 });
