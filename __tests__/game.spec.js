8304616757d598bcb591ea83bc4cf0d74f68a0f3
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-11-13 23:03:32 -0500

    Convert tabs to spaces. XD

--- __tests__/game.spec.js
+++ __tests__/game.spec.js
@@ -2,112 +2,112 @@ jest.dontMock('./../src/game');
 jest.dontMock('lodash');
 
 describe("Game", () => {
-	let sut;
-	beforeEach(() => {
-		sut = require('./../src/game')();
-	});
+  let sut;
+  beforeEach(() => {
+    sut = require('./../src/game')();
+  });
 
-	describe("Given a new game", () => {
-		it("Starts with a score of zero", () => {
-			expect(sut.getScore()).toEqual(0);
-		});
-	});
+  describe("Given a new game", () => {
+    it("Starts with a score of zero", () => {
+      expect(sut.getScore()).toEqual(0);
+    });
+  });
 
-	describe("Given a game with all gutter balls", () => {
-		beforeEach(() => {
-			for(var i = 0; i < 20; i++){
-				sut.roll(0);
-			}
-		});
+  describe("Given a game with all gutter balls", () => {
+    beforeEach(() => {
+      for(var i = 0; i < 20; i++){
+        sut.roll(0);
+      }
+    });
 
-		it('when calculating the score, then the score should equal 0', () => {
-			expect(sut.getScore()).toEqual(0);
-		});
-	});
+    it('when calculating the score, then the score should equal 0', () => {
+      expect(sut.getScore()).toEqual(0);
+    });
+  });
 
-	describe("Given a game with a 1's rolled", () => {
-		beforeEach(() => {
-			for(var i =0; i < 20; i++){
-				sut.roll(1)
-			}
-		});
+  describe("Given a game with a 1's rolled", () => {
+    beforeEach(() => {
+      for(var i =0; i < 20; i++){
+        sut.roll(1)
+      }
+    });
 
-		it('when calculating the score, then the score should equal 20', function(){
-				expect(sut.getScore()).toEqual(20);
-		});
-	});
+    it('when calculating the score, then the score should equal 20', function(){
+        expect(sut.getScore()).toEqual(20);
+    });
+  });
 
-	describe("Given a game with alternating 2 and 3 rolls", () => {
-		beforeEach(() => {
-			for(var i =0; i < 10; i++){
-				sut.roll(2);
-				sut.roll(3);
-			}
-		});
+  describe("Given a game with alternating 2 and 3 rolls", () => {
+    beforeEach(() => {
+      for(var i =0; i < 10; i++){
+        sut.roll(2);
+        sut.roll(3);
+      }
+    });
 
-		it('when calculating the score, then the score should equal 50', function(){
-				expect(sut.getScore()).toEqual(50);
-		});
-	});
+    it('when calculating the score, then the score should equal 50', function(){
+        expect(sut.getScore()).toEqual(50);
+    });
+  });
 
-	describe("Given a game with one roll", () => {
-		beforeEach(() => {
-			sut.roll(2);
-		});
+  describe("Given a game with one roll", () => {
+    beforeEach(() => {
+      sut.roll(2);
+    });
 
-		it('when calculating the score, then the score should equal 0', function(){
-				expect(sut.getScore()).toEqual(0);
-		});
-	});
+    it('when calculating the score, then the score should equal 0', function(){
+        expect(sut.getScore()).toEqual(0);
+    });
+  });
 
-	describe("Given a game with a spare", () => {
-		beforeEach(() => {
-			sut.roll(3);
-			sut.roll(7);
-			sut.roll(4);
-			sut.roll(2);
-		});
+  describe("Given a game with a spare", () => {
+    beforeEach(() => {
+      sut.roll(3);
+      sut.roll(7);
+      sut.roll(4);
+      sut.roll(2);
+    });
 
-		it('when calculating the score, then the score should include the bonus', function(){
-				expect(sut.getScore()).toEqual(20);
-		});
-	});
+    it('when calculating the score, then the score should include the bonus', function(){
+        expect(sut.getScore()).toEqual(20);
+    });
+  });
 
-	describe("Given a game with a strike", () => {
-		beforeEach(() => {
-			sut.roll(10);
-			sut.roll(5);
-			sut.roll(3);
-		});
+  describe("Given a game with a strike", () => {
+    beforeEach(() => {
+      sut.roll(10);
+      sut.roll(5);
+      sut.roll(3);
+    });
 
-		it('when calculating the score, then the score should include the bonus', function(){
-				expect(sut.getScore()).toEqual(26);
-		});
-	});
+    it('when calculating the score, then the score should include the bonus', function(){
+        expect(sut.getScore()).toEqual(26);
+    });
+  });
 
-	describe("Given a turky", () => {
-		beforeEach(() => {
-			sut.roll(10);
-			sut.roll(10);
-			sut.roll(10);
-			sut.roll(5);
-			sut.roll(3);
-		});
+  describe("Given a turky", () => {
+    beforeEach(() => {
+      sut.roll(10);
+      sut.roll(10);
+      sut.roll(10);
+      sut.roll(5);
+      sut.roll(3);
+    });
 
-		it('when calculating the score, then the score should include the bonus', function(){
-				expect(sut.getScore()).toEqual(81);
-		});
-	});
+    it('when calculating the score, then the score should include the bonus', function(){
+        expect(sut.getScore()).toEqual(81);
+    });
+  });
 
-	describe("Given a perfect game", () => {
-		beforeEach(() => {
-			for(var i = 0; i < 12; i++){
-				sut.roll(10);
-			}
-		});
+  describe("Given a perfect game", () => {
+    beforeEach(() => {
+      for(var i = 0; i < 12; i++){
+        sut.roll(10);
+      }
+    });
 
-		it('when calculating the score, then the score should equal 300', function(){
-				expect(sut.getScore()).toEqual(300);
-		});
-	});
+    it('when calculating the score, then the score should equal 300', function(){
+        expect(sut.getScore()).toEqual(300);
+    });
+  });
 });
--- src/game.js
+++ src/game.js
@@ -1,37 +1,37 @@
 import _ from 'lodash';
 
 export default function() {
-	var rolls = [];
+  var rolls = [];
 
-	function isFrameClosed(i){ return rolls[i] && rolls[i+1]; }
+  function isFrameClosed(i){ return rolls[i] && rolls[i+1]; }
 
-	function frameScore(i){ return rolls[i] + rolls[i+1]; }
+  function frameScore(i){ return rolls[i] + rolls[i+1]; }
 
-	function isSpare(i){ return rolls[i] + rolls[i+1] === 10; }
+  function isSpare(i){ return rolls[i] + rolls[i+1] === 10; }
 
-	function spareBonus(i){ return rolls[i+2]; }
+  function spareBonus(i){ return rolls[i+2]; }
 
-	function isStrike(i){ return rolls[i] === 10; }
+  function isStrike(i){ return rolls[i] === 10; }
 
-	function strikeBonus(i){ return rolls[i+1] + rolls[i+2]; }
+  function strikeBonus(i){ return rolls[i+1] + rolls[i+2]; }
 
-	return {
-		getScore: function() {
-			let score = 0;
-			let frame = 0;
-			_.times(10, () => {
-				if(isSpare(frame))
-					score += 10 + spareBonus(frame);
-				else if(isStrike(frame)){
-					score += 10 + strikeBonus(frame);
-					frame--;
-				}
-				else if(isFrameClosed(frame))
-					score += frameScore(frame);
-				frame += 2;
-			});
-			return score;
-		},
-		roll: function(pins) { rolls.push(pins); }
-	}
+  return {
+    getScore: function() {
+      let score = 0;
+      let frame = 0;
+      _.times(10, () => {
+        if(isSpare(frame))
+          score += 10 + spareBonus(frame);
+        else if(isStrike(frame)){
+          score += 10 + strikeBonus(frame);
+          frame--;
+        }
+        else if(isFrameClosed(frame))
+          score += frameScore(frame);
+        frame += 2;
+      });
+      return score;
+    },
+    roll: function(pins) { rolls.push(pins); }
+  }
 }
