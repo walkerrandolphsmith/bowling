4607fe7c08d3e817ea62d53dff3ebb8cd48a4b55
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-11-13 22:35:36 -0500

    A spare adds the next frame's first roll to the previous frame's score.

--- src/game.js
+++ src/game.js
@@ -7,11 +7,17 @@ export default function() {
 
 	function frameScore(i){ return rolls[i] + rolls[i+1]; }
 
+	function isSpare(i){ return rolls[i] + rolls[i+1] === 10; }
+
+	function spareBonus(i){ return rolls[i+2]; }
+
 	return {
 		getScore: function() {
 			let score = 0;
 			let frame = 0;
 			_.times(10, () => {
+				if(isSpare(frame))
+					score += spareBonus(frame);
 				if(isFrameClosed(frame))
 					score += frameScore(frame);
 				frame += 2;
